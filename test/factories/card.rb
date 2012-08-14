9e12afabe9c027b57f30e51726e0f93d0c90485f
esl and jellis <a-team@rallydev.com>
2012-08-14 16:31:18 -0400

    modified carf factory to allow named cards

--- test/factories/card.rb
+++ test/factories/card.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :card do |f|
-    card_archetype = CardArchetype.find_or_create_by_name("Birds of Paradise")
+    archetype = "Forest"
+    card_archetype = CardArchetype.find_or_create_by_name(archetype)
     f.card_archetype_id card_archetype.id
   end
 end
--- test/unit/library_test.rb
+++ test/unit/library_test.rb
@@ -9,6 +9,6 @@ class LibraryTest < ActiveSupport::TestCase
   test "Create default library puts cards in the library" do
     library = Library.create_default_library
     assert_true library.cards.any?
-    assert_equal "Birds of Paradise", library.cards[0].card_archetype.name
+    assert_equal "Forest", library.cards[0].card_archetype.name
   end
 end
