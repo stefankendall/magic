82d7fdce499b6917027b3c31cdaaac8b4e42196d
stefankendall <stefankendall@gmail.com>
2012-08-14 16:22:31 -0400

    Prevented factorygirl from causing migrations to fail against a clean database

--- test/factories/card.rb
+++ test/factories/card.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :card do |f|
-    card_archetype = CardArchetype.find_or_create_by_name("Birds of Paradise")
-    f.card_archetype_id card_archetype.id
+    f.card_archetype_id {
+      CardArchetype.find_or_create_by_name("Birds of Paradise").id
+    }
   end
 end
