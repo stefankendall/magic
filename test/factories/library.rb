d81a1fd6627c7c8ee64175964f40f36bb59e81e6
esl and jellis <a-team@rallydev.com>
2012-08-15 10:57:41 -0400

    Implemented creating default libraries

--- test/factories/library.rb
+++ test/factories/library.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     factory :elf_deck do |e|
       after(:build) do |library|
         library.cards << FactoryGirl.create_list(:card, 16, :archetype => 'Forest')
-        library.cards << FactoryGirl.create_list(:card, 24, archetype: "Elvish Warrior")
+        library.cards << FactoryGirl.create_list(:card, 24, :archetype => 'Elvish Warrior')
       end
     end
   end
