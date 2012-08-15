731059c76f21c90da6569343752ceabfe09a154a
esl and jellis <a-team@rallydev.com>
2012-08-15 16:20:51 -0400

    Added next_phase method to increment the phase and increment the turn if appropriate.

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -2,4 +2,22 @@ class Turn < ActiveRecord::Base
   has_one :player
   belongs_to :game
   attr_accessible :count, :phase, :player
+
+  @@PHASES = ['upkeep', 'draw', 'main phase 1', 'main phase 2', 'end step']
+
+  def next_phase
+    current_index = @@PHASES.find_index phase
+    current_index = (current_index + 1) % @@PHASES.length
+
+    new_count = count
+    if should_increment_turn
+      new_count = count + 1
+    end
+
+    update_attributes :phase => @@PHASES[current_index], :count => new_count
+  end
+
+  def should_increment_turn
+    player.order == 2 && phase == 'end step'
+  end
 end
--- test/unit/turn_test.rb
+++ test/unit/turn_test.rb
@@ -2,8 +2,41 @@ require 'test_helper'
 
 class TurnTest < ActiveSupport::TestCase
   test "A saved turn can be retrieved" do
-    t = Turn.create(count:5)
+    t = Turn.create(count: 5)
     saved_turn = Turn.find(t.id)
     assert_equal 5, saved_turn.count
+  end
+
+  test "next_phase moves to the next phase" do
+    t = Turn.create(phase: 'upkeep', count: 1, player: Player.create(order: 1))
+    t.next_phase
+    t.reload
+    assert_equal "draw", t.phase
+    assert_equal 1, t.count
+  end
+
+  test "next_phase moves to the next turn if you're on the end step and it's player 2's turn" do
+    p1 = Player.create(order: 1)
+    p2 = Player.create(order: 2)
+
+    t = Turn.create(phase: 'end step', count: 1, player: p2)
+    t.next_phase
+    t.reload
+    assert "upkeep", t.phase
+    assert_equal 2, t.count
+  end
+
+  test "should increment turn during phase change if it's the second player's end step" do
+    p1 = Player.create(order: 1)
+    p2 = Player.create(order: 2)
+    t = Turn.create(phase: 'end step', count: 1, player: p2)
+    assert_true t.should_increment_turn
+  end
+
+  test "should not increment turn during phase change if it's not the second player's end step" do
+    p1 = Player.create(order: 1)
+    p2 = Player.create(order: 2)
+    t = Turn.create(phase: 'end step', count: 1, player: p1)
+    assert_false t.should_increment_turn
   end
 end
